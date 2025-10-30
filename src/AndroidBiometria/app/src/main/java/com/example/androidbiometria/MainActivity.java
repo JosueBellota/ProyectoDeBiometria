1b85f09dcf8756fd2b924ea1e4d8206bb9631065
Josue Bellota Ichaso <69655184+JosueBellota@users.noreply.github.com>
2025-10-30 19:31:55 +0100

    vincular sensor terminado

--- src/AndroidBiometria/app/src/main/java/com/example/androidbiometria/MainActivity.java
+++ src/AndroidBiometria/app/src/main/java/com/example/androidbiometria/MainActivity.java
@@ -649,7 +649,7 @@
             Log.d(ETIQUETA_LOG, "🔍 Buscando dispositivo leído del QR: " + nombreDispositivoQR);
             this.buscarEsteDispositivoBTLE(nombreDispositivoQR);
 
-            FirebaseAuth.getInstance().signOut();
+            //FirebaseAuth.getInstance().signOut();
 
             // ✅ Nueva URL hacia tu intranet
             String url = "https://proyectodebiometria.web.app/";
@@ -657,7 +657,7 @@
             Intent intent = new Intent(MainActivity.this, WebNodoActivity.class);
             intent.putExtra("url", url);
             startActivity(intent);
-            
+
         }
 
 
