e03fa2ce645395aecb572bf56ff6158f89ec35e4
Joel <jgarmar3@epsg.upv.es>
2025-11-20 03:22:22 +0100

    2

--- src/WEB/my-app/src/Home.js
+++ src/WEB/my-app/src/Home.js
@@ -1,19 +1,11 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import HeaderNoRegistrado from "./templates/HeaderNoRegistrado";
 
 function Home() {
-  const navigate = useNavigate();
-
   return (
-    <div className="container">
-      <h1>Bienvenido a Proyecto de Biometría</h1>
-      <div style={{ marginTop: "20px" }}>
-        <button onClick={() => navigate("/login")}>Iniciar Sesión</button>
-        <button onClick={() => navigate("/registro")} style={{ marginLeft: "10px" }}>
-          Registrarse
-        </button>
-      </div>
-    </div>
+    <>
+      <HeaderNoRegistrado />
+    </>
   );
 }
 
---
+++ src/WEB/my-app/src/templates/HeaderNoRegistrado.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "../css/main.css";
+import { Link } from "react-router-dom";
+
+export default function HeaderNoRegistrado() {
+  return (
+    <header className="headerNR">
+      <div className="headerNR-left">
+        <img src="/logo.svg" alt="Logo" className="headerNR-logo" />
+      </div>
+
+      <nav className="headerNR-nav">
+        <Link to="/info">INFORMACIÓN</Link>
+        <Link to="/incidencias">INCIDENCIAS</Link>
+        <Link to="/login">INICIA SESIÓN</Link>
+      </nav>
+
+      <div className="headerNR-right">
+        <Link to="/registro" className="headerNR-btn">
+          REGÍSTRATE
+        </Link>
+      </div>
+    </header>
+  );
+}
